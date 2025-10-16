c97cdf2f7e0ce5333653da1d045fc8b2b8313aa5
Akash <akashmagar7500@gmail.com>
2025-10-16 21:49:37 +0530

    added card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
@@ -29,6 +30,7 @@ export default function Home() {
           </Button>
         </div>
       </section>
+      <Card />
     </main>
   );
 }
